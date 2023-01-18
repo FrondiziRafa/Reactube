c9d51e40c4a9c73d3a0aef303e237f57d2519a96
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-01-18 10:52:35 -0300

    Fim

--- reactube/src/components/ChannelDetail.jsx
+++ reactube/src/components/ChannelDetail.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
@@ -11,26 +11,30 @@ const ChannelDetail = () => {
 
   const { id } = useParams();
 
-  console.log(channelDetail, videos)
+  useEffect(() => {
+    const fetchResults = async () => {
+      const data = await fetchFromAPI(`channels?part=snippet&id=${id}`);
 
-  useEffect(()=>{
-    fetchFromAPI(`channels?part="snippet&id=${id}`)
-      .then((data)=> setChannelDetail(data?.items[0]));
+      setChannelDetail(data?.items[0]);
 
-    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
-      .then((data)=> setVideos(data?.items));
-  },[id])
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
+
+      setVideos(videosData?.items);
+    };
+
+    fetchResults();
+  }, [id]);
 
   return (
     <Box minHeight="95vh">
-      <Box >
+      <Box>
       <div style={{
-        background:'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(206,3,184,1) 35%, rgba(0,212,255,1) 100%)',
+        background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
         zIndex: 10,
-        height:'300px'
+        height:'300px',
       }} 
       />
-      <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
       </Box>
       <Box display="flex" p="2">
         <Box sx={{ mr: { sm:'100px' }}} />
--- reactube/src/components/VideoDetail.jsx
+++ reactube/src/components/VideoDetail.jsx
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import ReactPlayer from 'react-player';
-import { Typography, Box, Stack } from '@mui/material';
-import { CheckCircle }  from '@mui/icons-material';
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-import { Videos } from './';
-import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
   const [videos, setVideos] = useState(null);
   const { id } = useParams();
 
-  useEffect(()=>{
-    fetchFromAPI(`videos?part=snippet,statistics$id=${id}`)
-      .then((data) => setVideoDetail(data.items[0]));
-    
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]))
+
     fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
-      .then((data) => setVideos(data.items));
-  },[id])
+      .then((data) => setVideos(data.items))
+  }, [id]);
 
-  if (!videoDetail?.snippet) return 'Loading...'
+  if(!videoDetail?.snippet) return '...Loading';
 
-  const { snippet:{ title, channelId, channelTitle }, statistic:{ viewCount, likeCount } } = videoDetail;
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
 
   return (
     <Box minHeight="95vh">
@@ -37,7 +37,7 @@ const VideoDetail = () => {
               <Link to={`/channel/${channelId}`}>
                 <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
                   {channelTitle}
-                  <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+                  <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
                 </Typography>
               </Link>
               <Stack direction="row" gap="20px" alignItems="center">
@@ -52,11 +52,11 @@ const VideoDetail = () => {
           </Box>
         </Box>
         <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
-          <Videos direction="column" />
+          <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default VideoDetail
+export default VideoDetail;
